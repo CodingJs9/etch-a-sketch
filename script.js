7cb2cface47c2d8d901c42fb2eab710ded311f63
Rithvik <rithvikr98@gmail.com>
2021-11-11 18:17:28 +0530

    Added JavaScript and made small modifications to HTML and CSS

---
+++ script.js
@@ -0,0 +1,34 @@
+const gridSize = document.querySelector('input')
+const createGridBtn = document.querySelector('#create-grid')
+const clearGridBtn = document.querySelector('#clear-grid')
+const gridContainer = document.querySelector('.grid-container')
+
+// Prevents the form from reloading the page
+document.querySelector('form').onsubmit = (e) => e.preventDefault()
+
+// Event Listeners
+createGridBtn.addEventListener('click', createGrid)
+clearGridBtn.addEventListener('click', clearGrid)
+
+// Functions
+function createGrid() {
+	if (gridSize.value > 64) return
+	clearGrid()
+	let numberOfCells = gridSize.value * gridSize.value
+	gridContainer.style.setProperty('--grid-size', gridSize.value)
+
+	for (let i = 0; i < numberOfCells; i++) {
+		let gridCell = document.createElement('div')
+		gridCell.addEventListener('mouseover', () => {
+			gridCell.classList.add('coloured')
+		})
+		gridContainer.appendChild(gridCell)
+	}
+	gridSize.value = null
+}
+
+function clearGrid() {
+	while (gridContainer.firstChild) {
+		gridContainer.removeChild(gridContainer.firstChild)
+	}
+}
