58246054de0f581959297679ea3ef8a8ba1fa202
Rithvik <rithvikr98@gmail.com>
2021-12-05 18:28:08 +0530

    Made small code changes

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-const gridSize = document.querySelector('input')
-const createGridBtn = document.querySelector('#create-grid')
-const clearGridBtn = document.querySelector('#clear-grid')
-const gridContainer = document.querySelector('.grid-container')
+const gridSize = document.querySelector('input');
+const createGridBtn = document.querySelector('#create-grid');
+const clearGridBtn = document.querySelector('#clear-grid');
+const gridContainer = document.querySelector('.grid-container');
 
 // Prevents the form from reloading the page
-document.querySelector('form').onsubmit = (e) => e.preventDefault()
+document.querySelector('form').onsubmit = (e) => e.preventDefault();
 
 // Event Listeners
-createGridBtn.addEventListener('click', createGrid)
-clearGridBtn.addEventListener('click', clearGrid)
+createGridBtn.addEventListener('click', createGrid);
+clearGridBtn.addEventListener('click', clearGrid);
 
 // Functions
 function createGrid() {
-	if (gridSize.value > 64) return alert('Grid size cannot be higher than 64')
-	clearGrid()
-	let numberOfCells = gridSize.value * gridSize.value
-	gridContainer.style.setProperty('--grid-size', gridSize.value)
+	if (gridSize.value > 64) return alert('Grid size cannot be higher than 64');
+	clearGrid();
+	let numberOfCells = gridSize.value * gridSize.value;
+	gridContainer.style.setProperty('--grid-size', gridSize.value);
 
 	for (let i = 0; i < numberOfCells; i++) {
-		let gridCell = document.createElement('div')
-		gridCell.addEventListener('mouseover', () => {
+		let gridCell = document.createElement('div');
+		gridCell.addEventListener('mouseover', () =>
 			gridCell.classList.add('coloured')
-		})
-		gridContainer.appendChild(gridCell)
+		);
+		gridContainer.appendChild(gridCell);
 	}
-	gridSize.value = null
+	gridSize.value = null;
 }
 
 function clearGrid() {
 	while (gridContainer.firstChild) {
-		gridContainer.removeChild(gridContainer.firstChild)
+		gridContainer.removeChild(gridContainer.firstChild);
 	}
 }
